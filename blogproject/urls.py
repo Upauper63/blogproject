c42a9b188660b92ff600439aba9900181331f13d
Upauper63 <syunufkin@gmail.com>
2021-02-04 21:56:10 +0900

    static関連ファイル-開発用設定

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+# dev
+from django.conf import settings
+from django.conf.urls.static import static
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('blog.urls')),
+# ]
+
+# dev
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
