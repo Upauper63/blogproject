ba273faabc8556e22c77d7b630b17c48f4e070d4
Upauper63 <syunufkin@gmail.com>
2021-02-05 23:05:46 +0900

    Categoryモデル追加

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
+    category = models.ForeignKey(Category, on_delete=CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
