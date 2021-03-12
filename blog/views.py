3e18afa9621b841a31b8522b830ab47d45740f95
Upauper63 <syunufkin@gmail.com>
2021-03-12 17:44:57 +0900

    GoogleAnalytics用script設定 レイアウト微修正 ページネーション設定

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from .models import Post
 class Index(ListView):
     template_name = 'index.html'
     model = Post
+    paginate_by = 5
+    queryset = Post.objects.order_by('-created_at')
 
 class Detail(DetailView):
     template_name = 'detail.html'
